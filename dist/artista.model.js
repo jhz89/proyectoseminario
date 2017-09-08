1551ebcdd81ae5a46b8bb065614607917adea04f
harold <haroldzulugav@gmail.com>
2017-09-08 18:01:26 -0500

    se crea la clase artista

---
+++ dist/artista.model.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Artista = /** @class */ (function () {
+    function Artista() {
+    }
+    return Artista;
+}());
+exports.Artista = Artista;
+//# sourceMappingURL=artista.model.js.map
